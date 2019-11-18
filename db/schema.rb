4ece662ebabb10bed6774b0fe00fc0cd25bd4973
Louis Lescure <louis.lescure@mail.mcgill.ca>
2019-11-18 15:18:59 +0100

    added columns to user model (last & first_name, address, username, contact) and their validations"

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_18_134423) do
+ActiveRecord::Schema.define(version: 2019_11_18_141406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,11 @@ ActiveRecord::Schema.define(version: 2019_11_18_134423) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
+    t.string "last_name"
+    t.string "first_name"
+    t.string "username"
+    t.string "contact"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
