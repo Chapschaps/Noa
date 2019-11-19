395621637e883b2ef6c0b9cd24a526f503666d83
Leo Chaput <leo.chaput@hotmail.fr>
2019-11-19 13:18:43 +0100

    adding a booking confirmation

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :animals do
     resources :bookings, only: [:new, :create]
+    get '/confirmation', to: 'animals#booking_confirmation', as: 'confirmation'
   end
   resources :bookings, only: [:show]
   resources :users, except: [:index, :show] do
