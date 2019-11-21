25ab7bc2c8e0d244be231e65475fd6f676e12a0a
Leo Chaput <leo.chaput@hotmail.fr>
2019-11-21 13:16:13 +0100

    changing design of buttons

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,3 +6,25 @@ import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the styleshe
 import { initMapbox } from '../plugins/init_mapbox';
 
 initMapbox();
+
+var animateButton = function(e) {
+
+  e.preventDefault;
+  //reset animation
+  e.target.classList.remove('animate');
+
+  e.target.classList.add('animate');
+  setTimeout(function(){
+    e.target.classList.remove('animate');
+  },10000000);
+};
+
+var bubblyButtons = document.getElementsByClassName("bubbly-button");
+
+for (var i = 0; i < bubblyButtons.length; i++) {
+  bubblyButtons[i].addEventListener('mouseover', animateButton, false);
+}
+
+for (var i = 0; i < bubblyButtons.length; i++) {
+  bubblyButtons[i].addEventListener('click', animateButton, false);
+}
